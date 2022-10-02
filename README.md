7166d10049c4bfaa7133febda36aa4483232dd20
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2022-10-02 13:24:27 +0300

    added readme file and updated some print out text. To be upgraded with further ideas in the near future

---
+++ README.md
@@ -0,0 +1,6 @@
+To be updated and upgraded:
+ideas:
+* send message to friend 
+* full webapp or desktop app
+* GUI
+* ...
--- main.py
+++ main.py
@@ -5,6 +5,6 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 if direction == "encode":
-    print(encrypt(text, shift))
+    print(f'Here is the encoded message "{encrypt(text, shift)}"')
 else:
-    print(decode(text, shift))
+    print(f'Here is the decoded message "{decode(text, shift)}"')
